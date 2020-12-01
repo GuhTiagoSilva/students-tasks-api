0ba6f0e94253d1f351038491291d42518ccdfb19
Gustavo Tiago <gustavo.tiago.gts@outlook.com>
2020-11-30 21:53:00 -0500

    First Commit

---
+++ src/main/java/br/com/gustavo/studentstasks/StudentsTasksApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.studentstasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavo/studentstasks/StudentsTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.studentstasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
